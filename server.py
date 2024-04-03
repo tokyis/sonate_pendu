e4f493df67cb5b4f7449e83376e3ae8892900f88
kyis <kevin.mailletcontoz@gmail.com>
2024-04-03 20:27:18 +0200

    Added initial game state and base templates

--- server.py
+++ server.py
@@ -1,8 +1,66 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import random
 
 app = Flask(__name__)
 
+DICTIONARY = 'dictionnaire.txt'
 
-@app.route("/")
+state = None
+dictionary = None
+keys = None
+
+
+class GameState:
+    def __init__(self, username='', word='', lives=0):
+        self.username = username.strip()
+        self.word = word
+        self.lives = lives
+        self.guesses = ''
+
+
+def get_dictionary(path):
+    """
+    :param path: The file path to the dictionary text file.
+    :return: The dictionary extracted from the file as a list of strings.
+    """
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        dictionary = []
+        for line in lines:
+            values = line.split(';')
+            if len(values) != 0:
+                dictionary.append(values[0])
+    return dictionary
+
+
+def build_keymap(dictionary):
+    keys = []
+    for word in dictionary:
+        for letter in word:
+            if letter not in keys:
+                keys.append(letter)
+    keys.sort()
+    return keys
+
+
+def get_random_word(dictionary):
+    return random.choice(dictionary)
+
+
+def initialize():
+    global state, dictionary, keys
+    state = GameState()
+    dictionary = get_dictionary(DICTIONARY)
+    keys = build_keymap(dictionary)
+
+
+@app.route('/')
 def home():
-    return "<p>Hello, World!</p>"
+    global state, dictionary, keys
+    if state is None:
+        state = GameState()
+        dictionary = get_dictionary(DICTIONARY)
+        keys = build_keymap(dictionary)
+    return render_template('home.html')
+
+
